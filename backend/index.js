3f39b2350478c0911132495ec56f1e00edc693d1
Veronika Palto <nikalalin@gmail.com>
2025-02-13 17:12:44 +0300

    Add consts to shared, update consts usage

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import { WebSocketServer } from "ws";
+import { DATA_TYPES } from '../shared/consts.js';
+
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws) => {
@@ -7,14 +9,14 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const data = JSON.parse(message);
 
-    if (data.type === 'chat') {
+    if (data.type === DATA_TYPES.CHAT) {
       // Broadcast chat messages
       wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify({ type: 'chat', message: data.message, sender: data.sender }));
         }
       });
-    } else if (data.type === 'draw') {
+    } else if (data.type === DATA_TYPES.DRAW) {
       // Broadcast drawing data
       wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
